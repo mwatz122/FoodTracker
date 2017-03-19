8e64fc77474904d8d96bf3872f9fcee1c49bec52
Matthew A. Watzman <mwatz122@gmail.com>
2017-03-19 16:39:13 -0400

    added image picker gesture recognition

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -22,6 +22,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     //MARK: Actions
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        
+    }
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
     }
@@ -29,6 +32,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
