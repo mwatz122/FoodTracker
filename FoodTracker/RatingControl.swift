ff87033f2ec27327778507eba9fbcd071a0ba06a
Matthew A. Watzman <mwatz122@gmail.com>
2017-03-26 13:36:20 -0400

    Added button to main storyboard

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -1,21 +1,31 @@
-//
-//  RatingControl.swift
-//  FoodTracker
-//
-//  Created by Matthew Watzman on 3/26/17.
-//  Copyright © 2017 Matthew Watzman. All rights reserved.
-//
-
 import UIKit
 
 class RatingControl: UIStackView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+ 
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
     }
-    */
-
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        // Create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+    }
+    
 }
